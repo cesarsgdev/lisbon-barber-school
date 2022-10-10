09ab00223ff40ed1d5ab7a8d2ebafcc620a54e7c
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-10-09 23:21:03 -0500

    Added json and urlencoded middlewares.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const certificates = require("./routes/certificates");
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/certificates", certificates);
 
 app.get("/", (req, res) => {
   res.send("Entry endpoint...");
